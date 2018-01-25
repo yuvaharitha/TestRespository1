dda3ca1761dc0058db70550ce39bb7d6897a8a97
Yuvaharitha <yuva_hari2003@yahoo.co.in>
2018-01-25 12:36:38 -0600

    Hello World committed

---
+++ SampleGitProject/src/CallHelloWorld.java
@@ -0,0 +1,8 @@
+
+public class CallHelloWorld {
+public static void main(String[] args) {
+	System.out.println("calling Hello World");
+	HelloWorld hw = new HelloWorld();
+	hw.callMe();
+}
+}
---
+++ SampleGitProject/src/HelloWorld.java
@@ -0,0 +1,7 @@
+
+public class HelloWorld {
+	public void callMe()
+	{
+	System.out.println("Hello world");
+	}
+}
