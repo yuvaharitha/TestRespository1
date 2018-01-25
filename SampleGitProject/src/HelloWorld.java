05ea54adeaf70ad5aadde876fcb7d565a2501583
Yuvaharitha <yuva_hari2003@yahoo.co.in>
2018-01-25 16:43:48 -0600

    Added String

--- SampleGitProject/src/HelloWorld.java
+++ SampleGitProject/src/HelloWorld.java
@@ -2,6 +2,8 @@
 public class HelloWorld {
 	public void callMe()
 	{
+		String s= "Hi! This is my first Github project";
 	System.out.println("Hello world");
+	System.out.println("Prinitng string"+s);
 	}
 }
